1571a151af04f29bf7155431b75294d561aa2351
delamita <delamitaduke@icloud.com>
2017-12-10 16:34:09 +0800

    Initial Commit

---
+++ Insgrame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Insgrame
+//
+//  Created by 费克翔 on 2017/12/10.
+//  Copyright © 2017年 Crazzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
